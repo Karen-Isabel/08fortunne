e54fcbcb746f44c4e69fae5ca56f85a872b1aaea
Daniel <drums.daniel.9@gmail.com>
2016-11-10 17:02:26 -0600

    Server con front-end y commit en git hub

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var mime = require('mime')
 
 //Importando configuraciones
 var config = require('./config/config');
@@ -8,13 +9,21 @@ var IP = config.IP,
 
 var server = http.createServer(function(req, res){
     var url = req.url;
-    if(req.url == "/"){
+    if(url == "/"){
         url = '/index.html'
     }
+
+    
+    
+
     console.log(`> Recurso solicitado> ${url}`);
     var filePath = './static' +     url;
     console.log(`> Se servirá archivo: ${filePath}`);
-    fs.readFile(filePath, 'utf8', function(err, content){
+
+    //Seleccionar el tipo de mime
+    var mimeType = mime.lookup(filePath);
+
+    fs.readFile(filePath, function(err, content){
         if(err){
             //Hubo un error
                 res.writeHead(500,{
@@ -25,9 +34,9 @@ var server = http.createServer(function(req, res){
         }else{
             //No hubo error
                 res.writeHead(200,{
-                    'Content-Type': 'text/html'
+                    'Content-Type': mimeType
                 });
-                console.log('Sirviendo index.html');
+                console.log(`> Sirviendo: ${filePath}`);
                 res.end(content);
         }
         });
